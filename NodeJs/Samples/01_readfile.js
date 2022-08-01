e6dad918d0274e5106ed6c8448104776ca9ce9f4
Giovanni Frison <ing.giovanni.frison@gmail.com>
2022-08-01 17:15:02 +0200

    01/08 nodejs webserver

---
+++ NodeJs/Samples/01_readfile.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+// // Synchronous
+// const textIn = fs.readFileSync("./dummy.txt", "utf-8");
+// console.log(textIn);
+// const textOut = "\n\rIm writing to a file!";
+// fs.appendFileSync("./dummy.txt", textOut);
+
+// // Asynchronous using callbacks
+// fs.readFile("./dummy_async.txt", "utf-8", (err, data) => console.log(data)); // first argument is usually always the potential errors
+// console.log("This will be execute before the callback!");
+
+// async callbacks can be nested
+fs.appendFile(
+  "./dummy_async.txt",
+  "\nAdded text in async mode",
+  "utf-8",
+  (err) => {
+    fs.readFile("./dummy_async.txt", "utf-8", (err, data) => {
+      console.log(data);
+    });
+  }
+); // first argument is usually always the potential errors
+console.log("This will be execute before the callback!");
